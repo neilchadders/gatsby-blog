6a28e51069fe87799cbc88aa17c5b9aae1c84465
Neil Chadwick <neilchadders@hotmail.com>
2023-05-16 08:08:15 +0100

    add function to viewmarkdown in gatsby-node.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,14 @@ export default ({ data }) => {
       <div>
         <h1>Hi </h1>
         <h4>{data.allMarkdownRemark.totalCount}</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <span>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </span>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
       </div>
 
     </Layout>
