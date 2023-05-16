08a44c92d28c071aaf6207413e3d7d6d5414e7c1
Neil Chadwick <neilchadders@hotmail.com>
2023-05-16 20:50:32 +0100

    delete last finction on gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,26 +1,14 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
-
-const { createFilePath } = require('gatsby-source-filesystem')
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
-  if (node.internal.type === 'MarkdownRemark') {
-
-    const slug = createFilePath({ node, getNode })
-
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
     createNodeField({
       node,
       name: `slug`,
-      value: slug
+      value: slug,
     })
   }
-}
-
-export.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  return graphql(` `)
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,66 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 import Layout from "../components/layout"
 
+const Title = styled.h1`
+  display: inline-block;
+`
 
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
 
+  &:hover {
+    color: #1dcaff;
+  }
+`
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
+
+const BlogBody = styled.div`
+  margin-bottom: 50px;
+`
 
 export default ({ data }) => {
   return (
     <Layout>
-
       <div>
-        <h1>Hi </h1>
-        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        <Title>Thoughts by Yihua</Title>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <span>
-              {node.frontmatter.title} - {node.frontmatter.date}
-            </span>
-            <p>{node.excerpt}</p>
-          </div>
+          <BlogBody key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+              </BlogTitle>
+            </BlogLink>
+            <p>{node.frontmatter.description || node.excerpt}</p>
+          </BlogBody>
         ))}
       </div>
-
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
           id
           frontmatter {
-            description
             title
-            date
+            date(formatString: "DD MMMM, YYYY")
+            description
           }
-          excerpt
+          fields {
+            slug
+          }
+          excerpt(truncate: true)
         }
       }
     }
